be0b090ee30e2678b8550bd58e6114cb7ad6695c
Pato-Loco-Dev <camargoluizfrancisco035@gmail.com>
2024-04-22 19:54:20 -0300

    Messgae

--- flutterzin/lib/main.dart
+++ flutterzin/lib/main.dart
@@ -1,12 +1,93 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    const Center(
-      child: Text(
-      'Olá Mundo!!!',
-      textDirection: TextDirection.ltr,
-    )
-    )
-  );
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const appTitle = 'Exercicio Flutter';
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
+        body: const MyCustomForm(),
+        
+        
+        
+        
+      ),
+    );
+  }
+}
+
+class MyCustomForm extends StatelessWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      
+      crossAxisAlignment: CrossAxisAlignment.start,
+      
+      children: <Widget>[
+        IconButton(
+            icon: const Icon(Icons.add_alert),
+            tooltip: 'Show Snackbar',
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('This is a snackbar')));
+            },
+          ),
+          
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Digite seu usuario:',
+            ),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Digite sua Senha:',
+            ),
+          ),
+        ),
+        Center(
+          child:
+          IconButton(
+            icon: const Icon(Icons.navigate_next),
+            tooltip: 'Go to the next page',
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute<void>(
+                builder: (BuildContext context) {
+                  return Scaffold(
+                    appBar: AppBar(
+                      title: const Text('Next page'),
+                    ),
+                    body: const Center(
+                      child: Text(
+                        'Chegou na outra tela!',
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    ),
+                  );
+                },
+              ));
+            },
+            
+          )),
+        
+          
+      ],
+    );
+  }
 }
