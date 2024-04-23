cd1d569a3d3c7040626e8aa329f664853b19a60e
Pato-Loco-Dev <camargoluizfrancisco035@gmail.com>
2024-04-22 22:40:45 -0300

    start

--- AppSaude/app_saude_flutter/lib/main.dart
+++ AppSaude/app_saude_flutter/lib/main.dart
@@ -1,20 +1,79 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const AppSaude());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class AppSaude extends StatelessWidget {
+  const AppSaude({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const appTitle = 'Umbrella Corp';
     return const MaterialApp(
+      title: appTitle,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: MyCustomForm(),
       ),
+    );
+  }
+}
+
+class MyCustomForm extends StatelessWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      
+      crossAxisAlignment: CrossAxisAlignment.start,
+      
+      children: <Widget>[
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Digite seu usuario:',
+            ),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Digite sua Senha:',
+            ),
+          ),
+        ),
+        Center(
+          child:
+          IconButton(
+            icon: const Icon(Icons.navigate_next),
+            tooltip: 'Go to the next page',
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute<void>(
+                builder: (BuildContext context) {
+                  return Scaffold(
+                    appBar: AppBar(
+                      title: const Text('Next page'),
+                    ),
+                    body: const Center(
+                      child: Text(
+                        'Chegou na outra tela!',
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    ),
+                  );
+                },
+              ));
+            },
+            
+          )),
+        
+          
+      ],
     );
   }
 }
