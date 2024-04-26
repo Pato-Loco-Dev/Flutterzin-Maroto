a51fee011eb1fcbacb427891b334b1c28e3bb106
Pato-Loco-Dev <camargoluizfrancisco035@gmail.com>
2024-04-26 20:48:12 -0300

    Adicionado os campos de texto na tela de consulta

--- AppSaude/app_saude_flutter/lib/consultas.dart
+++ AppSaude/app_saude_flutter/lib/consultas.dart
@@ -1,3 +1,6 @@
+
+
+
 import 'package:flutter/material.dart';
 
 const List<String> horarioManha = <String>['08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30'];
@@ -13,27 +16,92 @@ class Consultas extends StatefulWidget{
 }
 
 class ConsultasState extends State<Consultas> {
+
+  String motivoConsulta = '';
+  String nmPaciente = '';
+
+  TextEditingController motivoConsController = TextEditingController();
+  TextEditingController nmPacienteController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    motivoConsController.dispose();
+    nmPacienteController.dispose();
+    
+    super.dispose();
+  }
   String dropdownValue = horarioManha.first;
 
   @override
   Widget build(BuildContext context){
     
-    return  DropdownMenu<String>(
-      
-      menuHeight: 300,
+    return  Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget> [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6.0),
+            child: TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                return 'Digite o motivo da consulta!';
+                } else { motivoConsulta = value;
+                }
+                return null;
+              },
+              controller: motivoConsController,
+              decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Digite o motivo da consulta:',
+            ),
+          ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6.0),
+            child: SizedBox(
+              
+              child: TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                return 'Digite o nome do paciente!';
+                } else { nmPaciente = value;
+                }
+                return null;
+              },
+              controller: nmPacienteController,
+              decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Digite o nome do paciente:',
+            ),
+          ),
+            ),
 
-      initialSelection: horarioManha.first,
-      onSelected: (String? value) {
-        
-        setState(() {
-          dropdownValue = value!;
-        });
-      },
-      dropdownMenuEntries: horarioManha.map<DropdownMenuEntry<String>>((String value) {
-        return DropdownMenuEntry<String>(value: value, label: value);
-      }).toList(),
+            
+          ),
+          Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16.0),
+                child: Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        
+                      } 
+                    },
+                    style: ElevatedButton.styleFrom(backgroundColor: const Color.fromARGB(255, 255, 0, 0)),
+                    child: const Text('Agendar consulta', style: TextStyle(color: Color.fromRGBO(255, 255, 255, 1)) ),
+                  ),
+                ),
+              )
+        ]
+      ),
     );
   }
+
+  
 
 
 }
